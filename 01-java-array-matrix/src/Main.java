75777a2246a73f36be37c2fd9d27543a45f4a1f8
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-03-10 12:44:11 -0300

    add all necessary variables

---
+++ 01-java-array-matrix/src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] cities = {
+                "London",
+                "Madrid",
+                "New York",
+                "Buenos Aires",
+                "Asunción",
+                "São Paulo",
+                "Lima",
+                "Santiago de Chile",
+                "Lisboa",
+                "Tokio"
+        };
+
+        int[][] temperatures = {
+                {-2, 33},
+                {-3, 32},
+                {-8, 27},
+                {4, 37},
+                {6, 42},
+                {5, 43},
+                {0, 39},
+                {-7, 26},
+                {-1, 31},
+                {-10, 35}
+        };
+
+        String lowTempCity;
+        String highTempCity;
+        int lowTemp;
+        int highTemp;
+
+    }
+}
