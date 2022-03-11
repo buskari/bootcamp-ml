97acf2931aa5239a524135bf73e029dfbfc2248a
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-03-11 17:31:58 -0300

    add class Product

--- 04-project-heranca-encap-poli/src/PraticaExcecoes.java
+++ 04-project-heranca-encap-poli/src/PraticaExcecoes.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class PraticaExcecoes {
+public class PraticaExcecoes {
+    private final int a = 0;
+    private final int b = 300;
+
+    public int calculaRazao() {
+        try {
+            return this.a / this.b;
+        } catch (ArithmeticException e) {
+            throw new IllegalArgumentException("Nao pode ser divido por zero");
+        } finally {
+            System.out.println("Programa finalizada");
+        }
+    }
 }
---
+++ 04-project-heranca-encap-poli/src/Product.java
@@ -0,0 +1,34 @@
+public class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "\nName: " + this.name + "\nPrice: " + this.price;
+    }
+
+    public double calculate(int quantity) {
+        return this.price * quantity;
+    }
+}
