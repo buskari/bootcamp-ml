2df8cb7a08c92d4c24e58b2ed6a24e88e93e14e3
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-03-16 16:28:55 -0300

    sprint 2 finalizada

--- 07-projeto-integrador-oo/src/Fatura.java
+++ 07-projeto-integrador-oo/src/Fatura.java
@@ -11,5 +11,21 @@ import java.util.List;
 public class Fatura {
     private Cliente cliente;
     private List<Item> itens;
-    private BigDecimal total;
+    private BigDecimal total = BigDecimal.ZERO;
+
+    public Fatura(Cliente cliente, List<Item> itens) {
+        this.cliente = cliente;
+        this.itens = itens;
+        setTotal();
+    }
+
+    public void setTotal() {
+        BigDecimal quantidade;
+        BigDecimal subtotal;
+        for (Item item : itens) {
+            quantidade = BigDecimal.valueOf((item.getQuantidade()));
+            subtotal = item.getCustoUnitario().multiply(quantidade);
+            this.total = this.total.add(subtotal);
+        }
+    }
 }
--- 07-projeto-integrador-oo/src/Main.java
+++ 07-projeto-integrador-oo/src/Main.java
@@ -1,35 +1,57 @@
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Cliente> listaClientes = new ArrayList<>();
+        ArrayList<Cliente> clientes = new ArrayList<>();
+        ArrayList<Item> itens = new ArrayList<>();
         String id;
 
         Cliente fulano = new Cliente("1001", "fulano", "da silva");
         Cliente ciclano = new Cliente("1002", "ciclano", "pereira");
         Cliente beltrano = new Cliente("1003", "beltrano", "gonçalves");
 
-        listaClientes.add(fulano);
-        listaClientes.add(ciclano);
-        listaClientes.add(beltrano);
+        itens.add(
+                new Item("2000", "caderno", 1, BigDecimal.valueOf(29.90))
+        );
+        itens.add(
+                new Item("2001", "caneta", 3, BigDecimal.valueOf(9.90))
+        );
+        itens.add(
+                new Item("2002", "laptop", 1, BigDecimal.valueOf(3999.00))
+        );
+        itens.add(
+                new Item("2003", "laptop", 1, BigDecimal.valueOf(3999.00))
+        );
+        itens.add(
+                new Item("2004", "laptop", 1, BigDecimal.valueOf(3999.00))
+        );
 
-        listaClientes.forEach(System.out::println);
-        listaClientes.remove(fulano);
+        Fatura fatura = new Fatura(fulano, itens);
+
+        clientes.add(fulano);
+        clientes.add(ciclano);
+        clientes.add(beltrano);
+
+        clientes.forEach(System.out::println);
+        clientes.remove(fulano);
         System.out.println("-----------------------------------------------------");
-        listaClientes.forEach(System.out::println);
+        clientes.forEach(System.out::println);
 
         System.out.println("Entre com o ID do cliente: ");
         id = scanner.next();
 
-        for (Cliente cliente : listaClientes) {
+        for (Cliente cliente : clientes) {
             if (cliente.getId().equals(id)) {
                 System.out.println(cliente);
                 return;
             }
         }
 
-        System.out.println("Não existe um cliente com este ID");
+//        System.out.println("Não existe um cliente com este ID");
+
+        System.out.println(fatura);
     }
 }
